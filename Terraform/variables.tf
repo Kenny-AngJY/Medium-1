30b133e2495759ec9c6c01e3d6529f9df33d0d4a
Kenny-AngJY <52172768+Kenny-AngJY@users.noreply.github.com>
2023-07-05 11:08:57 +0800

    Update variables.tf
    > Add description & validation for variable "S3PathAthenaQuery"
    > Add description for variable "Limit"

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -15,11 +15,18 @@ variable "Email" {
 variable "S3PathAthenaQuery" {
   type = string
   default = "s3://athena-query-information/s3-access-logs-last-accessed-queries/"
+  description = "The S3 URI"
+
+  validation {
+    condition     = substr(var.S3PathAthenaQuery, -1, 1) == "/" && substr(var.S3PathAthenaQuery, 0, 5) == "s3://"
+    error_message = "The S3 URI must start with \"s3://\" and end with \"/\"."
+  }
 }
 
 variable "Limit" {
   type = number
   default = 10
+  description = "The number of days before flagging the S3 bucket out."
 }
 
 variable "Query" {
